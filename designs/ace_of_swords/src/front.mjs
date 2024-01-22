a965d45b24a4b531d4a3ead1dd8b9590edfa29ea
Genia Boeing <genia@gboeing.com>
2024-01-22 15:34:00 +0000

    Add front of ace of swords

--- designs/ace_of_swords/src/front.mjs
+++ designs/ace_of_swords/src/front.mjs
@@ -1,11 +1,20 @@
 export const front = {
   name: 'ace_of_swords.front',
-  measurements: ['neck', 'waist'],
+  measurements: ['waist', 'highBust', 'waistToArmpit', 'hpsToWaistFront', 'shoulderToShoulder'],
   draft: ({ Path, paths, Point, points, measurements, part }) => {
-    points.a = new Point(0, 0)
-    points.b = new Point(measurements.neck, measurements.neck)
-      .addCircle(5)
-      .setText(measurements.waist)
+    const shirringMod = 1.5
+    const waistLength = (measurements.waist * shirringMod) / 4
+    const neckLength = (measurements.shoulderToShoulder * shirringMod) / 4
+    const verticalLength =
+      measurements.waistToArmpit + (measurements.hpsToWaistFront - measurements.waistToArmpit) / 2
+
+    paths.front = new Path()
+      .move(new Point(0, 0))
+      .line(new Point(0, verticalLength))
+      .line(new Point(waistLength, verticalLength))
+      .line(new Point(waistLength, verticalLength - measurements.waistToArmpit))
+      .line(new Point(neckLength, 0))
+      .line(new Point(0, 0))
 
     return part
   },
